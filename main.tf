39ef0eb99175efdb0010963e388be9adbe4f7a0b
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-27 19:03:14 -0400

    See #10.  Refactor code.

--- main.tf
+++ main.tf
@@ -1,25 +1,16 @@
 terraform {
     cloud {
-      organization = "<TERRAFORM CLOUD ORGANIZATION NAME>"
+      organization = "signalroom"
 
         workspaces {
-            name = "<TERRAFORM CLOUD ORGANIZATION's WORKSPACE NAME>"
+            name = "snowflake-resources-workspace"
         }
   }
 
   required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.63.0"
-        }
         snowflake = {
             source  = "Snowflake-Labs/snowflake"
             version = "~> 0.94.1"
         }
     }
-}
-
-locals {
-  cloud                    = "AWS"
-  snowflake_secrets_prefix = "/snowflake_resource"
 }
