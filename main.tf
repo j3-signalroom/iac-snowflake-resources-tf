501fb5e5aabcaf03e983a0b8b15fb175d40fda63
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-28 18:22:34 -0400

    Resolved #20.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
       organization = "signalroom"
 
         workspaces {
-            name = "snowflake-resources-workspace-019"
+            name = "snowflake-resources-workspace-022"
         }
   }
 
