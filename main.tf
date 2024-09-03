39b5c9e60148fa2b90eb5019f8a9fc8dad562546
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-09-03 10:22:23 -0400

    See #4.  Initial commit.

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
   }
 
   required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.65.0"
+        }
         snowflake = {
             source  = "Snowflake-Labs/snowflake"
             version = "~> 0.94.1"
