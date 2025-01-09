bb5a9ee32e3f77b2ca8090d72d9890ab9f6e0c90
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-01-09 17:41:46 -0500

    Resolved #77.

--- main.tf
+++ main.tf
@@ -14,11 +14,11 @@ terraform {
   required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "~> 5.80.0"
+            version = "~> 5.83.0"
         }
         snowflake = {
             source  = "Snowflake-Labs/snowflake"
-            version = "~> 1.0.0"
+            version = "~> 1.0.1"
         }
     }
 }
